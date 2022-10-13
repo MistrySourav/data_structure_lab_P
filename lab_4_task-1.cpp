e57b8383754bc0d00fe4e5edbb4b6f9d7ac9a8b4
Sourav Mistry <113965711+MistrySourav@users.noreply.github.com>
2022-10-13 19:18:36 +0600

    Update lab_4_task-1.cpp

--- lab_4_task-1.cpp
+++ lab_4_task-1.cpp
@@ -2,61 +2,91 @@
 
 using namespace std;
 
+class student
+{
 public:
-{
+    string name;
+    int age;
+    float cgpa;
 
+    student()
+    {
 
-int age;
-float cgpa;
-string name;
+    }
 
-student(string name,int age,float cgpa)
-{
+    student(string name,int age ,float cgpa)
+    {
+        this->name=name;
+        this->age=age;
+        this->cgpa=cgpa;
 
-    this->name=name;
-    this->age=age;
-     this->cgpa=cgpa;
-}
-}
+    }
+};
+
 int main()
 {
-    int min_index=0 ;
-   student temp =0;
-    int temp=0;
-
-
     student s1("steve",73,3.8);
     student s2("halk",35,3.78);
     student s3("tony",33,3.88);
     student s4("natasha",28,3.867);
     student s5("fury",66,3.758);
+    student s6("john",46,3.78);
 
-    student Student={s1,s2,s3,s4,s5};
+    student arr[]={s1,s2,s3,s4,s5,s6};
+
+    cout<<"Given Details are : "<<endl;
+    for(int i=0;i<6;i++)
+    {
+          cout<<arr[i].name<<"  ";
+
+           cout<<arr[i].age<<"  ";
+
+           cout<<arr[i].cgpa<<"  ";
+
+           cout<<endl;
+    }
 
 
+    cout<<endl;
+
+    int min_index=0;
+
+    student temp;
 
     for(int i=0; i<6; i++)
         {
+
         min_index = i;
 
         for(int j=i+1; j<6; j++)
-            {
-            if(Student[min_index].cgpa > Student[j].cgpa)
-            {
+
+        {
+            if(arr[min_index].cgpa > arr[j].cgpa){
                 min_index = j;
             }
-            }
+        }
 
-        temp = Student[min_index];
-        Student[min_index] = Student[i];
-        Student[i] = temp;
-    }
+        temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
 
-    for(int i=0; i<6;i++){
-        cout<<Student[i]<<" ";
-    }
+     }
 
-    cout<<endl;
+
+         cout<<"After Sorting Details are : "<<endl;
+
+    for(int i=0; i<6;i++)
+        {
+          cout<<arr[i].name<<" ";
+
+
+        cout<<arr[i].age<<" ";
+
+        cout<<arr[i].cgpa<<" "<<endl;
+
+        }
+
+
 
     return 0;
 }
